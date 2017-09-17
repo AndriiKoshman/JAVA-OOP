505e1c75b986b9bd1266ad4601f1b784e5435721
AndriiKoshman <31927083+AndriiKoshman@users.noreply.github.com>
2017-09-18 00:35:46 +0300

    Update Group.java

--- lesson 3/Group.java
+++ lesson 3/Group.java
@@ -1,4 +1,4 @@
-package com.company;
+package homework.lesson4;
 
 import java.util.Arrays;
 
@@ -6,7 +6,7 @@ public class Group {
 
 
     private String groupName;
-    private Student [] studentsArray = new Student[10];
+    private Student[] studentsArray = new Student[10];
 
     public Group(String groupName) {
         super();
@@ -26,62 +26,48 @@ public class Group {
         sb.append("Group - " + this.groupName);
         sb.append(System.lineSeparator());
 
-        // if >1 student in group then sort group
-        int count = 0;
-        for(int k = 0; k < studentsArray.length; k++){
-            if(studentsArray[k] != null) {
-                count++;
-            }
-        }
-        System.out.println(count);
-        if (count > 1){
-            sortGroup();
-        }
-
         for (int i = 0; i < studentsArray.length; i++) {
-            if(studentsArray[i] != null) {
-                sb.append((i+1) + ") " + studentsArray[i]);
+            if (studentsArray[i] != null) {
+                sb.append((i + 1) + ") " + studentsArray[i]);
             } else {
-                sb.append((i+1) + ") Free");
+                sb.append((i + 1) + ") Free");
             }
             sb.append(System.lineSeparator());
         }
         return sb.toString();
     }
 
-    public void sortGroup (){
-        try {
-        Student[] sortedArray = studentsArray;
-        Arrays.sort(sortedArray);
+    public void sortGroup() {
+        Arrays.sort(studentsArray);
+    }
 
-            for (int i = 0; i < studentsArray.length; i++) {
-                if (sortedArray[i] != null) {
-                    System.out.println(sortedArray[i].getFullName());
-                }
-            }
-        }
-        catch(NullPointerException nE){
-            System.out.println(nE.getMessage());
-        }
+    public void sortByAge(){
+        Arrays.sort(studentsArray,Student.ageComparator);
+        toString();
+    }
+
+    public void sortByFirstname(){
+        Arrays.sort(studentsArray,Student.firstNameComparatot);
+        toString();
     }
 
     //add student to the group
     public void addStudent(Student student) throws MyException {
-            for (int i = 0; i < studentsArray.length; i++) {
-                if (studentsArray[i] == null) {
-                    studentsArray[i] = student;
-                    student.setGroup(this.groupName);
-                    System.out.println("--MSG: Student " + student.getFullName() + " added to the group " + this.groupName);
-                    return;
-                }
+        for (int i = 0; i < studentsArray.length; i++) {
+            if (studentsArray[i] == null) {
+                studentsArray[i] = student;
+                student.setGroup(this.groupName);
+                System.out.println("--MSG: Student " + student.getFullName() + " added to the group " + this.groupName);
+                return;
             }
+        }
         throw new MyException();
-       }
+    }
 
     //remove student from the group
     public void removeStudent(Student student) {
         for (int i = 0; i < studentsArray.length; i++) {
-            if(studentsArray[i] == student) {
+            if (studentsArray[i] == student) {
                 studentsArray[i].setGroup(null);
                 studentsArray[i] = null;
                 System.out.println("--MSG: Student " + student.getFullName() + " removed from group " + this.groupName);
@@ -93,19 +79,16 @@ public class Group {
 
     //find student in the group by name
     public void findStudent(String fullName) {
-            for (int i = 0; i < studentsArray.length; i++) {
-                try {
-                    String formatedFirst = studentsArray[i].getFullName().replaceAll("\\s+","").toUpperCase();
-                    String formatedSecond = fullName.replaceAll("\\s+","").toUpperCase();
-                    if (studentsArray[i] != null && formatedFirst.compareTo(formatedSecond) == 0) {
-                        System.out.println("--MSG: Student found in the group " + this.groupName + " - " + studentsArray[i]);
-                        return;
-                    }
-                }
-                catch(NullPointerException nE){
-                    System.out.println(nE.getMessage());
+        for (int i = 0; i < studentsArray.length; i++) {
+            if (studentsArray[i] != null) {
+                String formatedFirst = studentsArray[i].getFullName().replaceAll("\\s+", "").toUpperCase();
+                String formatedSecond = fullName.replaceAll("\\s+", "").toUpperCase();
+                if (formatedFirst.compareTo(formatedSecond) == 0) {
+                    System.out.println("--MSG: Student found in the group " + this.groupName + " - " + studentsArray[i]);
+                    return;
                 }
             }
-            System.out.println("--ERROR: Student " + fullName + " not found in the group " + this.groupName);
+        }
+        System.out.println("--ERROR: Student " + fullName + " not found in the group " + this.groupName);
     }
 }
