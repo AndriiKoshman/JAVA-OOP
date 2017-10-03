02066dc188d431139197995bb2d75234f708b68f
AndriiKoshman <31927083+AndriiKoshman@users.noreply.github.com>
2017-10-03 15:54:37 +0200

    Create Main.java

---
+++ lesson6/task5/Main.java
@@ -0,0 +1,10 @@
+package homeworks.lesson6;
+
+public class Main {
+
+    static public void main(String[]args){
+
+        FolderChecker.startChecker("C:/Users/Public/Downloads");
+        
+        }
+ }       
