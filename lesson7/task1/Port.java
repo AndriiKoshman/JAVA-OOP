a24ff7eb364e80856619517772726e848f92426d
AndriiKoshman <31927083+AndriiKoshman@users.noreply.github.com>
2017-10-06 15:42:08 +0200

    Create Port.java

---
+++ lesson7/task1/Port.java
@@ -0,0 +1,49 @@
+package homeworks.lesson7;
+
+import java.util.concurrent.Semaphore;
+
+public class Port {
+
+    private int load;
+    private Semaphore semaphore;
+
+    public Port(int load, int docks) {
+        super();
+        this.load = load;
+        this.semaphore = new Semaphore(docks);
+    }
+
+
+
+    public void setLoad(Ship ship) {
+
+        try {
+        semaphore.acquire();
+        Thread thisThread = Thread.currentThread();
+        for (;ship.getVolume() > 0;){
+                System.out.println("Volume:" + ship.getVolume() + " - " + thisThread.getName());
+                ship.setVolume(ship.getVolume() - 1);
+                 load ++;
+                Thread.sleep(500);
+            }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            semaphore.release();
+            this.load = load;
+        }
+
+
+    public int getLoad() {
+        return load;
+    }
+
+    @Override
+    public String toString() {
+        return "Dock{" +
+                "load=" + load +
+                ", semaphore=" + semaphore +
+                '}';
+    }
+}
